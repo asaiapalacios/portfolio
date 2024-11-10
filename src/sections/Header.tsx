00d155c9e98fb9a1185b001d4b6e0c647cde8833
Asaia Palacios <14948113+asaiapalacios@users.noreply.github.com>
2024-11-10 15:19:57 -0600

    Initialize project: Header section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,31 @@
-export const Navbar = () => {
-  return null;
+import Logo from "@/assets/logosaas.png";
+import Image from "next/image";
+import MenuIcon from "@/assets/menu.svg";
+
+export const Header = () => {
+  return (
+    <header className="sticky top-0">
+      <div className="flex justify-center items-center py-3 bg-black text-white text-sm">
+        <div className="inline-flex gap-1 items-center">
+          <p>Welcome to my page!</p>
+        </div>
+      </div>
+      <div className="py-5">
+        <div className="container">
+          <div className="flex items-center justify-between">
+            <Image src={Logo} alt="Logo" height={40} width={40} />
+            <MenuIcon className="h-5 w-5 md:hidden" />
+            <nav className="hidden md:flex gap-6 text-black/60 items-center">
+              <a href="#">About</a>
+              <a href="#">Showcase</a>
+              <a href="#">Goals</a>
+              <button className="bg-black text-white px-4 py-2 rounded-lg font-medium inline-flex align-items justify-center">
+                Contact me
+              </button>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
