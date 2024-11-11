fd399a38e54145b6e75b605a565a5f708c5a3eca
Asaia Palacios <14948113+asaiapalacios@users.noreply.github.com>
2024-11-11 17:32:16 -0600

    Ticker section to display skills

--- src/sections/LogoTicker.tsx
+++ src/sections/LogoTicker.tsx
@@ -1,93 +1,36 @@
-"use client";
-import acmeLogo from "@/assets/logo-acme.png";
-import quantumLogo from "@/assets/logo-quantum.png";
-import echoLogo from "@/assets/logo-echo.png";
-import celestialLogo from "@/assets/logo-celestial.png";
-import pulseLogo from "@/assets/logo-pulse.png";
-import apexLogo from "@/assets/logo-apex.png";
+import htmlLogo from "@/assets/logo-html-canva.png";
+import cssLogo from "@/assets/logo-css-canva.png";
+import javascriptLogo from "@/assets/logo-javascript-canva.png";
+import uiLogo from "@/assets/logo-ui-canva.png";
+import uxLogo from "@/assets/logo-ux-canva.png";
+import reactLogo from "@/assets/logo-react-canva.png";
 import Image from "next/image";
-import { motion } from "framer-motion";
 
 export const LogoTicker = () => {
   return (
-    <div className="py-8 md:py-12 bg-white">
+    <div className="bg-white">
       <div className="container">
         <div className="flex overflow-hidden [mask-image:linear-gradient(to_right,transparent,black,transparent)]">
-          <motion.div
-            className="flex gap-14 flex-none pr-14"
-            animate={{
-              translateX: "-50%",
-            }}
-            transition={{
-              duration: 20,
-              repeat: Infinity,
-              ease: "linear",
-              repeatType: "loop",
-            }}
-          >
+          <div className="flex gap-20 flex-none">
             <Image
-              src={acmeLogo}
-              alt="Acme Logo"
+              src={htmlLogo}
+              alt="HTML Logo"
               className="logo-ticker-image"
             />
+            <Image src={cssLogo} alt="CSS Logo" className="logo-ticker-image" />
             <Image
-              src={quantumLogo}
-              alt="Quantum Logo"
+              src={javascriptLogo}
+              alt="JavaScript Logo"
               className="logo-ticker-image"
             />
+            <Image src={uxLogo} alt="UX Logo" className="logo-ticker-image" />
+            <Image src={uiLogo} alt="UI Logo" className="logo-ticker-image" />
             <Image
-              src={echoLogo}
-              alt="Echo Logo"
+              src={reactLogo}
+              alt="React Logo"
               className="logo-ticker-image"
             />
-            <Image
-              src={celestialLogo}
-              alt="Celestial Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={pulseLogo}
-              alt="Pulse Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={apexLogo}
-              alt="Apex Logo"
-              className="logo-ticker-image"
-            />
-
-            {/* Second set of logos for animation */}
-            <Image
-              src={acmeLogo}
-              alt="Acme Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={quantumLogo}
-              alt="Quantum Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={echoLogo}
-              alt="Echo Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={celestialLogo}
-              alt="Celestial Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={pulseLogo}
-              alt="Pulse Logo"
-              className="logo-ticker-image"
-            />
-            <Image
-              src={apexLogo}
-              alt="Apex Logo"
-              className="logo-ticker-image"
-            />
-          </motion.div>
+          </div>
         </div>
       </div>
     </div>
